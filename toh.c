a72032e757a1e55c0f9089f171b06eefc6339500
Shashank Verma <71495535+shashankdasverma@users.noreply.github.com>
2021-04-19 12:58:47 +0545

    Create toh.c

---
+++ toh.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+
+void towers(int, char, char, char);
+int hcf(int n1, int n2);
+
+int main()
+{
+    int num;
+    printf("Enter the number of disks : ");
+    scanf("%d", &num);
+    printf("The sequence of moves involved in the Tower of Hanoi are :\n");
+    towers(num, 'A', 'C', 'B');
+    printf("\n");
+    int n1, n2;
+    printf("\nEnter two positive integers: ");
+    scanf("%d %d", &n1, &n2);
+    printf("G.C.D of %d and %d is %d.", n1, n2, hcf(n1, n2));
+    return 0;
+}
+void towers(int num, char frompeg, char topeg, char auxpeg)
+{
+    if (num == 1)
+    {
+        printf("\n Move disk 1 from peg %c to peg %c", frompeg, topeg);
+        return;
+    }
+    towers(num - 1, frompeg, auxpeg, topeg);
+    printf("\n Move disk %d from peg %c to peg %c", num, frompeg, topeg);
+    towers(num - 1, auxpeg, topeg, frompeg);
+}
+
+
+
+
+
+int hcf(int n1, int n2) {
+    if (n2 != 0)
+        return hcf(n2, n1 % n2);
+    else
+        return n1;
+}
